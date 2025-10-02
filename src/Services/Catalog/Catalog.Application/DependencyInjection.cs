4ff6405dbfda2c5c054d2d82937962bda2f65f93
mcbec <mc.becna@gmail.com>
2025-10-02 12:27:19 +0300

    refactoring

--- src/Services/Catalog/Catalog.Application/DependencyInjection.cs
+++ src/Services/Catalog/Catalog.Application/DependencyInjection.cs
@@ -4,7 +4,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddValidatorsFromAssemblyContaining<GetCatalogItemByIdQuery>();
+        services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
         
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
         
