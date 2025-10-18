2353babca1ea63be4bc27b7415b3c09e7635e45b
mcbec <mc.becna@gmail.com>
2025-10-19 00:29:54 +0300

    feat: add FluentValidation support to custom exception handler

--- src/Common/Common.Kernel/Exceptions/Handler/CustomExceptionHandler.cs
+++ src/Common/Common.Kernel/Exceptions/Handler/CustomExceptionHandler.cs
@@ -1,3 +1,4 @@
+using FluentValidation;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -11,6 +12,7 @@ public class CustomExceptionHandler : IExceptionHandler
         var (title, detail, statusCode) = exception switch
         {
             NotFoundException ex => ("Ресурс не найден", ex.Message, StatusCodes.Status404NotFound),
+            ValidationException ex => ("Ошибка валидации", ex.Message, StatusCodes.Status400BadRequest),
             _ => ("Внутренняя ошибка сервера", "Произошла непредвиденная ошибка.", StatusCodes.Status500InternalServerError)
         };
         
@@ -26,6 +28,20 @@ public class CustomExceptionHandler : IExceptionHandler
         
         problemDetails.Extensions.Add("traceId", httpContext.TraceIdentifier);
 
+        if (exception is ValidationException validationException)
+        {
+            var errors = validationException.Errors?.Select(x => new {x.PropertyName, x.ErrorMessage}).ToList();
+
+            if (errors is not null && errors.Count > 0)
+            {
+                problemDetails.Extensions.Add("ValidationException", errors);
+            }
+        }
+        else
+        {
+            problemDetails.Extensions.Add("errorMessage", exception.Message);
+        }
+        
         await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
 
         return true;
