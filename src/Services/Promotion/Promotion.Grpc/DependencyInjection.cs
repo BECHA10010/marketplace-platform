ca26c3fbb11a5716786d15529831c073dc21bc9a
mcbec <mc.becna@gmail.com>
2025-11-03 21:36:03 +0300

    (feat(Promotion.Grpc): basic service setup - add a Promo domain model - filling database with DatabaseExtensions.SeedAsync - move using directives to GlobalUsings)

---
+++ src/Services/Promotion/Promotion.Grpc/DependencyInjection.cs
@@ -0,0 +1,34 @@
+namespace Promotion.Grpc;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        var mySqlConnection = configuration.GetConnectionString("MySqlConnection");
+
+        services.AddScoped<IDbConnection>(_ => new MySqlConnection(mySqlConnection));
+        
+        services.AddGrpcReflection();
+        services.AddGrpc();
+
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+        });
+        
+        return services;
+    }
+    
+    public static async Task<WebApplication> UseApiServices(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var connection = scope.ServiceProvider.GetRequiredService<IDbConnection>();
+        
+        await connection.SeedAsync();
+        app.MapGrpcReflectionService();
+
+        app.MapGrpcService<GreeterService>();
+        
+        return app;
+    }
+}
---
+++ src/Services/Promotion/Promotion.Grpc/Persistence/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,34 @@
+namespace Promotion.Grpc.Persistence.Extensions;
+
+public static class DatabaseExtensions
+{
+    public static async Task SeedAsync(this IDbConnection connection)
+    {
+        if (connection.State != ConnectionState.Open)
+        {
+            connection.Open();
+        }
+        
+        const string dropTableSql = "DROP TABLE IF EXISTS Promo";
+        await connection.ExecuteAsync(dropTableSql);
+        
+        const string createTableSql = $"""
+                                      CREATE TABLE IF NOT EXISTS Promo (
+                                        Id CHAR(36) NOT NULL PRIMARY KEY,
+                                        CatalogItemId VARCHAR(255) NOT NULL,
+                                        Title VARCHAR(255) NOT NULL,
+                                        Value DECIMAL(18,2) NOT NULL
+                                      );
+                                      """;
+        await connection.ExecuteAsync(createTableSql);
+
+        var promos = InitData.GetInitialPromos;
+
+        const string insertSql = $"""
+                                  INSERT INTO Promo (Id, CatalogItemId, Title, Value)
+                                  VALUES (@Id, @CatalogItemId, @Title, @Value);
+                                  """;
+        
+        await connection.ExecuteAsync(insertSql, promos);
+    }
+}
